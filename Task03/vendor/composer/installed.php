6375e76cb6c3e6782cc6350b2d212dd7b9252d83
Andrey Popov <andrvpopov@gmail.com>
2022-02-05 10:01:16 +0300

    Add task01

---
+++ Task03/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '1e4513564df70110ff5113231e24e9e4bf6df7f8',
+    'name' => 'dksenia/tic-tac-toe',
+  ),
+  'versions' => 
+  array (
+    'dksenia/tic-tac-toe' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1e4513564df70110ff5113231e24e9e4bf6df7f8',
+    ),
+    'wp-cli/php-cli-tools' => 
+    array (
+      'pretty_version' => 'v0.11.13',
+      'version' => '0.11.13.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a2866855ac1abc53005c102e901553ad5772dc04',
+    ),
+  ),
+);
